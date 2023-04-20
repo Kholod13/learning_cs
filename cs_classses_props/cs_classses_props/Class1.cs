ee0da2eeddee24cbcf8437202725a3c45ca1f645
Kholod13 <111706893+Kholod13@users.noreply.github.com>
2023-04-20 10:53:44 +0300

    add homework "cs_classes_props"

---
+++ cs_classses_props/cs_classses_props/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_classses_props
+{
+    public partial class Class1
+    {
+        public int p1 { get; set; }
+        public int p2 { get; set; }
+        public void Swap(ref Class1 a1, ref Class1 a2)
+        {
+            Class1 temp = a1;
+            a1 = a2;
+            a2 = temp;
+        }
+
+        public void InputProperties(out int p1, out int p2)
+        {
+            Console.Write($"Input property 1 >> ");
+            int.TryParse( Console.ReadLine(), out p1 );
+
+            Console.Write($"Input property 2 >> ");
+            int.TryParse(Console.ReadLine(), out p2);
+
+        }
+    }
+}
---
+++ cs_classses_props/cs_classses_props/Program.cs
@@ -0,0 +1,90 @@
+﻿using System.Reflection;
+
+namespace _cs_classes_props
+{
+    internal class Program
+    {
+        public class Phone
+        {
+            private static double inches;
+            private string color;
+            private int battery;
+            private static double cameraMegaPixels;
+            private readonly string company;
+
+            public static double Inches { get { return inches; } set { inches = value; } }
+            public string Color { get { return color; } set { color = value; } }
+            public int Battery { get { return battery;} set { battery = value; } }
+            public int BatteryPower { get; set ; }
+            public static double CameraMegaPixels { get { return cameraMegaPixels; } set { cameraMegaPixels = value; } }
+            public string Company { get { return company;} }
+            public bool Power { get; set; }
+
+            public Phone(double inches, string color, int battery, double cameraMegaPixels, string company)
+            {
+                Inches = inches;
+                Color = color;
+                Battery = battery;
+                CameraMegaPixels = cameraMegaPixels;
+                this.company = company;
+                BatteryPower = battery;
+            }
+
+            static Phone() { 
+                Inches= 0;
+                CameraMegaPixels = 0;
+            }
+            public Phone(string company)
+            {
+                Inches = 0;
+                Color = "";
+                Battery = 0;
+                CameraMegaPixels = 0;
+                this.company = company;
+            }
+            public void PowerPhone() {
+                if (Power == false) {
+                    Power= true;
+                    Console.WriteLine("Your phone is ON!");
+                }
+                else
+                {
+                    Power = false;
+                    Console.WriteLine("Your phone is OFF!");
+                }
+            }
+
+            public void TakePhoto() {
+                Console.WriteLine("Photo done!");
+            }
+            public void UsePhone()
+            {
+                Console.Write("Input number minutes using >> ");
+                int min = int.Parse(Console.ReadLine());
+
+                int p = min / 6;
+
+                int a = BatteryPower / 100 * p;
+
+                BatteryPower = BatteryPower - a;
+                Console.WriteLine($"{BatteryPower}mah left, with({Battery})");
+            }
+            public void EditBattery(ref int value)
+            {
+                Battery = value;
+            }
+
+        }
+        void Main(string[] args)
+        {
+            Phone[] phone =
+            {
+                new Phone("Xiaomi"),
+                new Phone("Apple"),
+                new Phone("Huawei"),
+                new Phone("Samsung"),
+                new Phone("Nokia"),
+            };
+        }
+    }
+}
