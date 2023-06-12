b1a7d2ca4eead8f0530e1e12141c818578b8ca8d
Kholod13 <111706893+Kholod13@users.noreply.github.com>
2023-06-12 17:09:34 +0300

    upload hw "oop_serialization"

---
+++ oop_serialization/oop_serialization/MagazineManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_serialization
+{
+    [Serializable]
+    internal class MagazineManager : IEnumerable
+    {
+        private List<Magazine> _magazines = new List<Magazine>();
+
+        public void AddMagazine(Magazine magazine)
+        {
+            _magazines.Add(magazine);
+        }
+        public void DisplayMagazines()
+        {
+            foreach (var magazine in _magazines)
+            {
+                magazine.DisplayInfo();
+                Console.WriteLine();
+            }
+        }
+        public void SirializeMagazine(string filename)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            using (Stream fs = File.Create(filename))
+            {
+                formatter.Serialize(fs, _magazines);
+            }
+        }
+        public void DeserializeMagazine(string filename)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (Stream fs = File.OpenRead(filename))
+            {
+                _magazines = (List<Magazine>)formatter.Deserialize(fs);
+            }
+        }
+        public void ClearMagazine()
+        {
+            _magazines.Clear();
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return ((IEnumerable)_magazines).GetEnumerator();
+        }
+    }
+}
---
+++ oop_serialization/oop_serialization/MyFloat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_serialization
+{
+    struct MyFloat
+    {
+        public int Numerator { get; set; }
+        public int Denominator { get; set; }
+
+        public MyFloat(int numerator, int denominator)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+        public override string ToString()
+        {
+            return $"{Numerator}/{Denominator}";
+        }
+    }
+}
